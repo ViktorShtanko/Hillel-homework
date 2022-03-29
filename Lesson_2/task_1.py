128e9353fab11fa7391e8069f005b6af139282bc
Viktor Shtanko <viktorshtanko@Viktors-MacBook-Pro.local>
2022-03-29 22:11:42 +0300

    all tasks

--- Lesson_2/task_1.py
+++ Lesson_2/task_1.py
@@ -7,14 +7,8 @@ print(my_list1)
 my_list3=[10,11,2,3,5,8,23,11,2,5,76,43,2,32,76,3,10,0,1]
 my_list4=list(set(my_list3))
 my_list5=sorted(my_list4)
-x=max(my_list5)
-print(x)
-del my_list5[-1]
-y=max(my_list5)
-print(y)
-del my_list5[-1]
-z=max(my_list5)
-print(z)
+my_list6=my_list5[-3:]
+print(my_list6)
 
 # 1.3) вывести индекс минимального элемента массива
 a=min(my_list1)
---
+++ task_2.py
@@ -0,0 +1,5 @@
+new_list =list(range(101))
+for num in new_list:
+    if num % 2 == 0:
+        print(new_list[num], end = ',')
+
